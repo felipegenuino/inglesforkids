a9cfb562e0d7d145114cf19a01f30475447d5284
Felipe Genuino <design@felipegenuino.com>
2024-12-26 03:44:42 -0300

    Initial Commit

---
+++ inglesforkids/inglesforkidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  inglesforkidsApp.swift
+//  inglesforkids
+//
+//  Created by Felipe Genuino on 26/12/24.
+//
+
+import SwiftUI
+
+@main
+struct inglesforkidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
